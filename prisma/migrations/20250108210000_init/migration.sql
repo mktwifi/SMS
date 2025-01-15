e0284ed8ab893a72a74defc8d6064f26f1087a30
mktwifi <108961786+mktwifi@users.noreply.github.com>
2025-01-16 00:09:57 +0500

    MY test

---
+++ prisma/migrations/20250108210000_init/migration.sql
@@ -0,0 +1,127 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Admin` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Announcement` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Assignment` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Attendance` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Class` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Event` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Exam` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Grade` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Lesson` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Parent` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Result` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Student` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Subject` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Teacher` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_SubjectToTeacher` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Announcement" DROP CONSTRAINT "Announcement_classId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Assignment" DROP CONSTRAINT "Assignment_lessonId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Attendance" DROP CONSTRAINT "Attendance_lessonId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Attendance" DROP CONSTRAINT "Attendance_studentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Class" DROP CONSTRAINT "Class_gradeId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Class" DROP CONSTRAINT "Class_supervisorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Event" DROP CONSTRAINT "Event_classId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Exam" DROP CONSTRAINT "Exam_lessonId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Lesson" DROP CONSTRAINT "Lesson_classId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Lesson" DROP CONSTRAINT "Lesson_subjectId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Lesson" DROP CONSTRAINT "Lesson_teacherId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Result" DROP CONSTRAINT "Result_assignmentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Result" DROP CONSTRAINT "Result_examId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Result" DROP CONSTRAINT "Result_studentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Student" DROP CONSTRAINT "Student_classId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Student" DROP CONSTRAINT "Student_gradeId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Student" DROP CONSTRAINT "Student_parentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_SubjectToTeacher" DROP CONSTRAINT "_SubjectToTeacher_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_SubjectToTeacher" DROP CONSTRAINT "_SubjectToTeacher_B_fkey";
+
+-- DropTable
+DROP TABLE "Admin";
+
+-- DropTable
+DROP TABLE "Announcement";
+
+-- DropTable
+DROP TABLE "Assignment";
+
+-- DropTable
+DROP TABLE "Attendance";
+
+-- DropTable
+DROP TABLE "Class";
+
+-- DropTable
+DROP TABLE "Event";
+
+-- DropTable
+DROP TABLE "Exam";
+
+-- DropTable
+DROP TABLE "Grade";
+
+-- DropTable
+DROP TABLE "Lesson";
+
+-- DropTable
+DROP TABLE "Parent";
+
+-- DropTable
+DROP TABLE "Result";
+
+-- DropTable
+DROP TABLE "Student";
+
+-- DropTable
+DROP TABLE "Subject";
+
+-- DropTable
+DROP TABLE "Teacher";
+
+-- DropTable
+DROP TABLE "_SubjectToTeacher";
+
+-- DropEnum
+DROP TYPE "Day";
+
+-- DropEnum
+DROP TYPE "UserSex";
